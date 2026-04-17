86d4d839af8f6265f21f2ba6562bc1cb51698fcf
incldue <h52463398@outlook.com>
2026-04-18 01:12:28 +0800

    Site updated: 2026-04-18 01:12:26

--- js/main.js
+++ js/main.js
@@ -248,3 +248,17 @@ onBeforeContentReplace(() => {
 onVisitStart(() => {
   navbarShrink.setNavigating(true);
 });
+
+var OriginTitle = document.title;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        document.title = '╭(°A°`)╮ 你去哪了? 快回来!!!';
+    }else {
+        document.title = '(ฅ>ω<*ฅ) 你终于回来了 ~';
+        setTimeout(function () {
+            if(!document.hidden){
+                document.title = OriginTitle;
+            }
+        }, 2000);
+    }
+});
